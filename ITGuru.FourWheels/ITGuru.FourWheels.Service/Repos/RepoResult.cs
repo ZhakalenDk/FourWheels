ea75a50f0d12ed670e936b1a145a0f7bfeed5a41
Mike Starup Mortensen <Zhakalen@Gmail.com>
2022-09-27 17:06:01 +0200

    Improved error handling between data and service

--- ITGuru.FourWheels/ITGuru.FourWheels.Service/Repos/RepoResult.cs
+++ ITGuru.FourWheels/ITGuru.FourWheels.Service/Repos/RepoResult.cs
@@ -6,9 +6,9 @@
         {
             Message = message;
         }
-        public string Message { get; }
+        public string Message { get; set; }
 
-        public Exception Exception { get; }
+        public Exception Exception { get; set; } = null;
 
         public bool Succeeded => Exception != null;
     }
