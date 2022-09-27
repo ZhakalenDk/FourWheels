e2af2467ca74a29f3483613768a4e362012a8e96
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2022-09-27 09:45:38 +0200

    ~Changed Testpage.cshtml.cs to use new .NET 6 namespace format

--- ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Testpage.cshtml.cs
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Testpage.cshtml.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ITGuru.FourWheels.Web.Pages
-{
+namespace ITGuru.FourWheels.Web.Pages;
     public class TestpageModel : PageModel
     {
         public void OnGet()
         {
         }
     }
-}
