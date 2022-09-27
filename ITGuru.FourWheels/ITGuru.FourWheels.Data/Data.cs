d9e97f4156a34ddaad1a397a0ce136bbd4faaee1
Jens Anker Bruun de Neergaard <jens5561@elevcampus.dk>
2022-09-27 13:34:11 +0200

    + Added Person + Added Customer + Added Data + Created Data-Mock

---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Data/Data.cs
@@ -0,0 +1,79 @@
+﻿using ITGuru.FourWheels.Data.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITGuru.FourWheels.Data
+{
+    public class Data
+    {
+        List<Customer> customers = new();
+
+        public Data()
+        {
+            GenerateList();
+        }
+
+        public void GenerateList()
+        {
+            Guid g = Guid.NewGuid();
+            Customer customer = new Customer()
+            {
+                Id = g,
+                FirstName = "jens",
+                LastName = "Neergaard",
+                Email = "jensneergaard@hotmail.com",
+                Phone = "12345678"
+            };
+            customers.Add(customer);
+
+            Guid g2 = Guid.NewGuid();
+            Customer customer2 = new Customer()
+            {
+                Id = g2,
+                FirstName = "Mike",
+                LastName = "Mortensen",
+                Email = "MikeMortensen@hotmail.com",
+                Phone = "23456789"
+            };
+
+            customers.Add(customer2);
+
+            Guid g3 = Guid.NewGuid();
+            Customer customer3 = new Customer()
+            {
+                Id = g3,
+                FirstName = "Lukas",
+                LastName = "Pederson",
+                Email = "LukasPederson@hotmail.com",
+                Phone = "34567890"
+            };
+            customers.Add(customer3);
+
+            Guid g4 = Guid.NewGuid();
+            Customer customer4 = new Customer()
+            {
+                Id = g4,
+                FirstName = "Simon",
+                LastName = "Andreassen",
+                Email = "SimonAndreassen@hotmail.com",
+                Phone = "23456789"
+            };
+            customers.Add(customer4);
+
+            Guid g5 = Guid.NewGuid();
+            Customer customer5 = new Customer()
+            {
+                Id = g5,
+                FirstName = "Jasmin",
+                LastName = "Nielsen",
+                Email = "JasminNielsen@hotmail.com",
+                Phone = "23456789"
+            };
+            customers.Add(customer5);
+        }
+
+    }
+}
---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Data/DataModels/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ITGuru.FourWheels.Data.DataModels
+{
+    public class Person
+    {
+        public Guid Id;
+        public string FirstName;
+        public string LastName;
+    }
+}
