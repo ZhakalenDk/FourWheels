a45702f82428d0807d4f9bb58b7741142d10088e
Mike Starup Mortensen <Zhakalen@Gmail.com>
2022-09-27 18:53:40 +0200

    Added an extensions for the IServiceCollection to add IData as a Singleton instance

---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Service/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ITGuru.FourWheels.Data.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ITGuru.FourWheels.Service
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IData"/> as a <strong>Singleton</strong> instance
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddContext(this IServiceCollection services)
+        {
+            return services.AddSingleton<IData, Data.Data>();
+        }
+    }
+}
