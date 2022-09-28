3af683b6d20c463d235c38bbc90d3b063ca19dbd
Mike Mortensen <mike2137@elevcampus.dk>
2022-09-28 11:51:34 +0200

    Added service interfaces and DTO's
    Note: No logic is included

---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Service/DTOs/VehicleDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITGuru.FourWheels.Service
+{
+    public class VehicleDTO : IVehicle
+    {
+        public Guid Id { get; }
+        public string LicensePlate { get; }
+        public string Brand { get; }
+        public string Model { get; }
+        public Guid CustomerId { get; }
+    }
+}
