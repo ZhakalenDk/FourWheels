0b287f92554c2d2091abeda971f7467e9427af95
Jens Anker Bruun de Neergaard <jens5561@elevcampus.dk>
2022-09-28 14:30:48 +0200

    missing Interface update to reflect changes

--- ITGuru.FourWheels/ITGuru.FourWheels.Data/Interfaces/IDataLayer.cs
+++ ITGuru.FourWheels/ITGuru.FourWheels.Data/Interfaces/IDataLayer.cs
@@ -5,12 +5,12 @@ namespace ITGuru.FourWheels.Data
     public interface IDataLayer
     {
         void GenerateList();
-        List<Customer> GetAllCustomers(bool getDeleted);
+        List<Customer> GetAllCustomers(bool includeDeleted);
         bool AddCustomer(Customer customer);
         bool UpdateCustomer(Customer customer);
         bool HardDeleteCustomer(Guid customerID);
         bool SoftDeleteCustomer(Guid customerID);
-        List<Vehicle> GetAllVehicles(bool getDeleted);
+        List<Vehicle> GetAllVehicles(bool includeDeleted);
         bool AddVehicle(Vehicle vehicle);
         bool UpdateVehicle(Vehicle vehicle);
         bool HardDeleteVehicle(Guid vehicleID);
