eb4640c5c92d7ec659f8bc86bbe1e1e45dd7d18a
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2022-09-28 11:29:14 +0200

    Initializing for CRUD bug fix

---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Customer/DeleteCustomer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITGuru.FourWheels.Web.Pages.Administration.Customer
+{
+    public class DeleteCustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Customer/UpdateCustomer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITGuru.FourWheels.Web.Pages.Administration.Customer
+{
+    public class UpdateCustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
