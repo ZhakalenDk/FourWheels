810114700db0a3d1d1318faceebba114c63245c0
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2022-09-30 11:27:45 +0200

    +Added Task remove

--- ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Tasks/DeleteTask.cshtml.cs
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Tasks/DeleteTask.cshtml.cs
@@ -1,3 +1,5 @@
+using ITGuru.FourWheels.Service;
+using ITGuru.FourWheels.Web.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,36 @@ namespace ITGuru.FourWheels.Web.Pages.Administration.Tasks
 {
     public class DeleteTaskModel : PageModel
     {
-        public void OnGet()
+        private readonly ITaskService _taskService;
+        public DeleteTaskModel(ITaskService taskService)
         {
+            _taskService = taskService;
+        }
+
+        [BindProperty]
+        public TaskDTO Task { get; set; }
+
+
+
+        public void OnGet(string taskId)
+        {
+            Task = _taskService.GetById(new Guid(taskId)) as TaskDTO;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (_taskService.Remove(Task).Succeeded)
+            {
+                TempData["Message"] = $"Successfully delete the task {Task.Description}";
+                TempData["MessageStatus"] = MessageStatus.Success;
+                return Redirect($"/Administration/Tasks/VehicleTasks/{Task.AssociatedVehicleId}");
+            }
+            else
+            {
+                TempData["Message"] = $"Couldn't delete task {Task.Description}";
+                TempData["MessageStatus"] = MessageStatus.Failed;
+                return Redirect($"/Administration/Tasks/VehicleTasks/{Task.AssociatedVehicleId}");
+            }
         }
     }
 }
