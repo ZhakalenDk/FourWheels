8688c04c759b34371e1e7d9453aa547a6b30501d
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2022-09-30 09:09:26 +0200

    +Added update task page +Added delete task page +Added vehicle tasks page

---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Tasks/DeleteTask.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITGuru.FourWheels.Web.Pages.Administration.Tasks
+{
+    public class DeleteTaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Tasks/VehicleTasks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITGuru.FourWheels.Web.Pages.Administration.Tasks
+{
+    public class VehicleTasksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
