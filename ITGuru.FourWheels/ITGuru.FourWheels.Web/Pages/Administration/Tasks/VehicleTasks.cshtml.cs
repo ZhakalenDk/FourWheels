0be079dea14968676c16919a03c94bd62734baee
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2022-09-30 09:32:37 +0200

    VehicleTasks page ready for interface implementation

--- ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Tasks/VehicleTasks.cshtml.cs
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/Pages/Administration/Tasks/VehicleTasks.cshtml.cs
@@ -1,3 +1,4 @@
+using ITGuru.FourWheels.Web.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,19 @@ namespace ITGuru.FourWheels.Web.Pages.Administration.Tasks
 {
     public class VehicleTasksModel : PageModel
     {
-        public void OnGet()
+        //TODO: Uncomment when ITask interface is done
+        //[BindProperty]
+        //public IReadOnlyList<ITask> Tasks { get; set; }
+
+        [BindProperty]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public MessageStatus MessageStatus { get; set; }
+
+        public void OnGet(string VehicleId)
         {
+
         }
     }
 }
