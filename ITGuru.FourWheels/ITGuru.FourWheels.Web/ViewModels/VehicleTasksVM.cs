d11a3e4e1a7efee1a9036374b113e2640d98d6e7
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2022-09-30 12:03:33 +0200

    creating new task works

---
+++ ITGuru.FourWheels/ITGuru.FourWheels.Web/ViewModels/VehicleTasksVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITGuru.FourWheels.Web.ViewModels
+{
+    public class VehicleTasksVM
+    {
+        public Guid VehicleId { get; set; }
+        public Guid TaskId { get; set; }
+        [Required]
+        public string OrderNumber { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string LicensePlate { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
